7918c011634df6c1318ae89c8b0585442fcf505a
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-08-04 14:01:21 -0400

    Add shelf divs

--- bookshelf-2/src/Bkshelf1.js
+++ bookshelf-2/src/Bkshelf1.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./index.css";
 
 export default function Bkshelf1() {
-  return <h1>This is where the books will go</h1>;
+  return (
+    <div className="shelf-container">
+      <h1>New & Trending</h1>
+      <div className="book-container">
+        <img src="../public/reader-favicon.png" />;
+      </div>
+    </div>
+  );
 }

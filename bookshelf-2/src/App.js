c0773ff4475983d383f8e917dfafb9b508046567
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-08-03 22:55:27 -0400

    create react app

---
+++ bookshelf-2/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./Navbar.js";
+import Shelf from "./Shelf.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Shelf />
+      {/* bookshelf-1 */}
+      {/* shelf */}
+      {/* bkshelf-2
+      shelf */}
+      {/* <header className="App-header">
+        
+      </header> */}
+    </div>
+  );
+}
+
+export default App;

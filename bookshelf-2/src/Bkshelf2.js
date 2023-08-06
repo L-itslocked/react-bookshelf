e47f27476fc78eae6173c55b17b02f5f2f32a0a8
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-08-05 21:15:06 -0400

    Add ID and sample text

---
+++ bookshelf-2/src/Bkshelf2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Bkshelf2() {
+  return (
+    <div id="booktok">
+      <p>"Hi!"</p>;
+    </div>
+  );
+}
---
+++ bookshelf-2/src/Bkshelf3.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Bkshelf3() {
+  return (
+    <div id="staff-picks">
+      <p>"Howdy from Shelf 3!"</p>;
+    </div>
+  );
+}

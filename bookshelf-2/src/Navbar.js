efd6058c7619836c130b9e90a412f775f776203a
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-08-05 21:15:39 -0400

    Update className

--- bookshelf-2/src/Navbar.js
+++ bookshelf-2/src/Navbar.js
@@ -3,10 +3,10 @@ import "./Nav.css";
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarTogglerDemo01"
@@ -14,44 +14,44 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a className="navbar-brand" href="#">
             <h4>Bookery</h4>
           </a>
-          <ul class="navbar-nav me-auto mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#bestsellers">
+          <ul className="navbar-nav me-auto mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#bestsellers">
                 Bestsellers
               </a>
             </li>
-            {/* <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#audiobooks"
+            {/* <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#audiobooks"
                   >Audiobooks</a
                 >
               </li> */}
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#booktok">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#booktok">
                 #Booktok
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#staff-picks">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#staff-picks">
                 Staff Picks
               </a>
             </li>
           </ul>
         </div>
         <ul id="nav-icons-ul">
-          <li class="nav-icons nav-link">
+          <li className="nav-icons nav-link">
             <a href="#">
-              <i class="fa-solid fa-bookmark"></i>
+              <i className="fa-solid fa-bookmark"></i>
             </a>
           </li>
-          <li class="nav-icons nav-link">
+          <li className="nav-icons nav-link">
             <a href="#">
-              <i class="fa-solid fa-user"></i>
+              <i className="fa-solid fa-user"></i>
             </a>
           </li>
         </ul>

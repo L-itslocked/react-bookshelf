b07d70f7368a1ba41856e901ef26eb461ce59783
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-08-09 12:40:32 -0400

    Create Footer.js

---
+++ bookshelf-2/src/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/L-itslocked/react-bookshelf">
+        Open-source code
+      </a>{" "}
+      by Laura Doyon
+      <div>
+        <a href="https://www.linkedin.com/in/doyonlaura">
+          <i class="fa-brands fa-linkedin coder-social"></i>
+        </a>
+        <a href="https://github.com/L-itslocked">
+          <i class="fa-brands fa-github coder-social"></i>
+        </a>
+        <a href="https://codepen.io/Itslocked" target="_blank">
+          <i class="fa-brands fa-codepen coder-social"></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
